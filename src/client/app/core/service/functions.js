76085be04681baa06b14f6dceeb543cba93b583d
Juan Benjumea <juanbenjumea@gmail.com>
2017-08-25 07:20:04 -0500

    - Crear función para calcular el ancho de las columnas en lines

--- src/client/app/core/service/functions.js
+++ src/client/app/core/service/functions.js
@@ -14,7 +14,54 @@
         return service;
 
         function columns(total) {
+            /* COLUMNS */
 
+            var regular_col_md = 0;
+            var special_col_md = 0;
+            var regular = 0;
+            var special = 0;
+            var div_3 = parseInt(total / 3);
+            var res_3 = total % 3;
+            var div_4 = parseInt(total / 4);
+            var res_4 = total % 4;
+
+            if(total === 1){
+                regular_col_md = 'col-md-6';
+                regular = 1;
+            }
+            else if(total === 2){
+                regular_col_md = 'col-md-6';
+                regular = 2;
+            }
+            else if(res_4 === 0){
+                regular_col_md = 'col-md-3';
+                regular = total;
+            }
+            else if(res_3 === 0){
+                regular_col_md = 'col-md-4';
+                regular = total;
+            }
+            else if(res_3 > res_4){ // Ej: 5
+                regular = div_3*3;
+                special = res_3;
+                regular_col_md = 'col-md-4';
+                special_col_md = 'col-md-'+12/res_3;
+            }
+            else { // 10 Ej: 10
+                regular = div_4*4;
+                special = res_4;
+                regular_col_md = 'col-md-3';
+                special_col_md = 'col-md-'+12/res_4;
+            }
+
+            return {
+                'regular': regular,
+                'special': special,
+                'regular_col_md': regular_col_md,
+                'special_col_md': special_col_md
+            };
+
+            /*
             var rows = 1;                           // Number of regular rows
             var colsRows = 0;                      // Col width in regular rows
             var colsRowFinal = 0;                 // Col width in final row
@@ -50,7 +97,7 @@
                 'rows': rows,
                 'cols_rows': colsRows,
                 'cols_row_final': colsRowFinal
-            };
+            };*/
         }
     }
 })();
--- src/client/app/sport/controller/sport.line.controller.js
+++ src/client/app/sport/controller/sport.line.controller.js
@@ -6,10 +6,17 @@
         .controller('SportLineController', SportLineController);
 
     /* @ngInject */
-    function SportLineController(lines, sport) {
+    function SportLineController(lines, sport, functions) {
         /*jshint unused:false*/
         var vm = this;
         vm.sport = sport.data;
         vm.lines = lines;
+        
+        var cols = functions.columns(lines.length);
+
+        vm.regular = cols.regular; 
+        vm.special = cols.special; 
+        vm.regular_col_md = cols.regular_col_md;
+        vm.special_col_md = cols.special_col_md;
     }
 })();
